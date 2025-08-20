8111e9cd419bc12b8b7554f5e7a110e24d805de3
LuisC70 <luiscaceres7500@gmail.com>
2025-08-19 22:41:52 -0500

    Tiempo Lineal O(n)

---
+++ Lineal.java
@@ -0,0 +1,13 @@
+public class Lineal {
+    public static void main(String[] args) {
+        int[] numeros = {10, 20, 30, 40, 50};
+        
+        System.out.println("O(n) - Tiempo lineal:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Elemento en posición " + i + ": " + numeros[i]);
+        }
+    }
+}
+
+    
+
