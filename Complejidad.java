eea4c76a7757edca9cc3010df523a5c107f36936
LuisC70 <luiscaceres7500@gmail.com>
2025-08-21 23:30:09 -0500

    (Temporal ; Espacial)

---
+++ Complejidad.java
@@ -0,0 +1,13 @@
+public class Complejidad {
+    public static void main(String[] args) {
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        
+        int valor = numeros[2]; 
+
+        System.out.println("Elemento en la posición 2: " + valor);
+    }
+}
+
+    
+
