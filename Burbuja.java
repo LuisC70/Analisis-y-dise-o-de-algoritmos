ca6e6c65a043ee0f58bf72547851460a8e16517f
LuisC70 <luiscaceres7500@gmail.com>
2025-08-21 23:24:10 -0500

    Por burbuja

--- Burbuja.java
+++ Burbuja.java
@@ -7,11 +7,11 @@ public class Burbuja {
             System.out.print(num + " ");
         }
 
-        // Algoritmo Burbuja
+        
         for (int i = 0; i < numeros.length - 1; i++) {
             for (int j = 0; j < numeros.length - 1 - i; j++) {
                 if (numeros[j] > numeros[j + 1]) {
-                    // Intercambiar
+                    
                     int temp = numeros[j];
                     numeros[j] = numeros[j + 1];
                     numeros[j + 1] = temp;
