b6440c19571fd602482611035b7c0f3fc0be2e11
LuisC70 <luiscaceres7500@gmail.com>
2025-08-24 23:53:28 -0500

    Proyect _Analisis

--- Proyecto_Analisis.java
+++ Proyecto_Analisis.java
@@ -1,3 +1,30 @@
 public class Proyecto_Analisis {
+    public static void main(String[] args) {
+        int[] numeros = {5, 2, 9, 1, 5, 6};
+
+        System.out.println("Arreglo original:");
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+
+        
+        for (int i = 0; i < numeros.length - 1; i++) {
+            for (int j = 0; j < numeros.length - 1 - i; j++) {
+                if (numeros[j] > numeros[j + 1]) {
+                    
+                    int temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\n\nArreglo ordenado con Burbuja:");
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+    }
     
 }
+    
+
