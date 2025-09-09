64f7e7e20e09cfe4a8f001ae79c000bfd24188f8
LuisC70 <luiscaceres7500@gmail.com>
2025-09-09 08:44:48 -0500

    Estilo_Polifasico

---
+++ Estilo_Polifasico.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Estilo_Polifasico {
+    
+                                                                           // Mezcla dos listas ordenadas en una nueva lista
+    public static List<Integer> merge(List<Integer> a, List<Integer> b) {
+        List<Integer> result = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < a.size() && j < b.size()) {
+            if (a.get(i) <= b.get(j)) {
+                result.add(a.get(i));
+                i++;
+            } else {
+                result.add(b.get(j));
+                j++;
+            }
+        }
+
+                                                                             // Añadimos los elementos que sobren
+        while (i < a.size()) result.add(a.get(i++));
+        while (j < b.size()) result.add(b.get(j++));
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+                                                                             // Secuencias iniciales simuladas (runs ya ordenados)
+        List<Integer> run1 = Arrays.asList(1, 4, 7);
+        List<Integer> run2 = Arrays.asList(2, 5, 8);
+        List<Integer> run3 = Arrays.asList(3, 6, 9);
+        List<Integer> run4 = Arrays.asList(0, 10, 11);
+
+                                                                             // Primera fase de mezcla
+        List<Integer> temp1 = merge(run1, run2);
+        List<Integer> temp2 = merge(run3, run4);
+
+        System.out.println("Primera mezcla:");
+        System.out.println(temp1);
+        System.out.println(temp2);
+
+                                                                             // Segunda fase de mezcla
+        List<Integer> finalResult = merge(temp1, temp2);
+
+        System.out.println("Resultado final ordenado:");
+        System.out.println(finalResult);
+    }
+}
+
+
+    
+
---
+++ Proyecto_Actualizado.java
@@ -0,0 +1,21 @@
+public class Proyecto_Actualizado{
+       public static void main(String[] args) {
+        
+        int Num = 5;
+
+        if (Num >= 0) {
+            System.out.println("El numero es Entero");
+        
+        } else {
+            System.out.print("El numero no es entero");
+        }
+    }
+} 
+ 
+
+//Para la programacion adquerida
+
+
+  
+
+
